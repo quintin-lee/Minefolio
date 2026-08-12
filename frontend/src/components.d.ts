f591e4256a992229c4dcff4c82876e7522230726
quintin <2449164582@qq.com>
2026-08-12 13:38:42 +0800

    chore: update auto-imports (ElAlert component)

--- frontend/src/components.d.ts
+++ frontend/src/components.d.ts
@@ -36,6 +36,7 @@ declare module 'vue' {
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElProgress: typeof import('element-plus/es')['ElProgress']
+    ElRadio: typeof import('element-plus/es')['ElRadio']
     ElRadioButton: typeof import('element-plus/es')['ElRadioButton']
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElRow: typeof import('element-plus/es')['ElRow']
