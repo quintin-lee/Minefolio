1eef7539d4872b9b49332d55246f9fedf584b3c6
quintin <2449164582@qq.com>
2026-08-23 18:23:19 +0800

    fix: use custom base64url encode to avoid csilk framework crash

--- backend/src/config/key_manager.c
+++ backend/src/config/key_manager.c
@@ -1,5 +1,4 @@
 #include "common/response.h"
-#include "csilk/core/codec.h"
 #include "csilk/core/crypto_dispatch.h"
 #include "csilk/drivers/cipher.h"
 #include <openssl/bio.h>
@@ -30,6 +29,27 @@ const char* auth_key_get_public_pem(void)  { return g_pub_pem; }
 const char* auth_key_get_private_pem(void) { return g_priv_pem; }
 size_t auth_key_get_private_pem_len(void)  { return g_priv_pem_len; }
 
+/* Base64url encode manually */
+static void base64url_encode(const uint8_t* src, size_t len, char* dst) {
+    static const char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    size_t i = 0, j = 0;
+    uint32_t val = 0;
+    int bit = 0;
+    
+    for (i = 0; i < len; i++) {
+        val = (val << 8) + src[i];
+        bit += 8;
+        while (bit >= 6) {
+            bit -= 6;
+            dst[j++] = table[(val >> bit) & 0x3f];
+        }
+    }
+    if (bit > 0) {
+        dst[j++] = table[(val << (6 - bit)) & 0x3f];
+    }
+    dst[j] = '\0';
+}
+
 /* Extract JWK n,e from RSA public key */
 static int rsa_pubkey_to_jwk(const char* pub_pem, size_t pub_pem_len, char* jwk_buf, size_t jwk_cap) {
     BIO* bio = BIO_new_mem_buf(pub_pem, (int)pub_pem_len);
@@ -58,14 +78,15 @@ static int rsa_pubkey_to_jwk(const char* pub_pem, size_t pub_pem_len, char* jwk_
     BN_bn2bin(n, nbuf);
     BN_bn2bin(e, ebuf);
     
+    /* Strip leading zeros */
     size_t ni = 0;
     while (ni < (size_t)nlen && nbuf[ni] == 0) ni++;
     size_t ei = 0;
     while (ei < (size_t)elen && ebuf[ei] == 0) ei++;
     
     char n_b64[360], e_b64[8];
-    csilk_base64url_encode(nbuf + ni, (size_t)nlen - ni, n_b64);
-    csilk_base64url_encode(ebuf + ei, (size_t)elen - ei, e_b64);
+    base64url_encode(nbuf + ni, (size_t)nlen - ni, n_b64);
+    base64url_encode(ebuf + ei, (size_t)elen - ei, e_b64);
     free(nbuf);
     free(ebuf);
     
