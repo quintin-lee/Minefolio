fd11d35a8b6c9736013bcef7942792056322ecf7
quintin <2449164582@qq.com>
2026-09-04 13:09:11 +0800

    refactor(cashflow): 🎯 strangle legacy cashflow layer and rewire main to DDD interfaces

--- backend/src/controllers/cashflow_controller.c
+++
@@ -1,9 +0,0 @@
-/**
- * @file cashflow_controller.c
- * @brief 被动现金流计划与日历预测控制器实现文件
- *
- * 路由注册与 HTTP 适配由 interfaces/http/controllers/cashflow_controller.c 实现。
- */
-
-#include "controllers/cashflow_controller.h"
-#include "interfaces/http/controllers/cashflow_controller.h"
--- backend/src/main.c
+++ backend/src/main.c
@@ -22,7 +22,7 @@
 #include "controllers/import_export_controller.h"
 #include "interfaces/http/controllers/market_controller.h"
 #include "controllers/dca_controller.h"
-#include "controllers/cashflow_controller.h"
+#include "interfaces/http/controllers/cashflow_controller.h"
 #include "controllers/ledger_controller.h"
 #include "controllers/import_rule_controller.h"
 #include "controllers/receipt_controller.h"
