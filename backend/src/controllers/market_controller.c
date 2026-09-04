e1ac56f91058cf2d6672631d52fb3a284c6c75e6
quintin <2449164582@qq.com>
2026-09-04 12:44:58 +0800

    refactor(market): 🎯 remove legacy market controller and service

--- backend/src/controllers/market_controller.c
+++
@@ -1,9 +0,0 @@
-/**
- * @file market_controller.c
- * @brief 市场行情数据、汇率服务与定时同步配置控制器实现文件
- *
- * 路由注册与 HTTP 适配由 interfaces/http/controllers/market_controller.c 实现。
- */
-
-#include "controllers/market_controller.h"
-#include "interfaces/http/controllers/market_controller.h"
